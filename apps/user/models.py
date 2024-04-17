1c723c7a6a0a30bfa96c83df02c5a16918e9fa83
Aracely Heath <aracelyv19@gmail.com>
2024-04-17 17:08:02 -0400

    added email,password, and updated at

--- apps/user/models.py
+++ apps/user/models.py
@@ -11,4 +11,7 @@ class User(models.Model):
         db_table = 'user'
 
     name = models.CharField('Name', max_length = 50, blank=False, null=False) 
+    email = models.CharField('Email', max_length = 50, blank=False, null=False)
+    password = models.CharField('Password', max_length=50, blank=False, null=False)
+    updated_at = models.DateTimeField('Update_at',  blank=True, auto_now_add=True)
     created_at = models.DateTimeField('Created_at', blank=True, auto_now_add=True)
