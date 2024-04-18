8c5883c27285798e981e635d96c9d6291df9b8d0
Aracely Heath <aracelyv19@gmail.com>
2024-04-18 00:25:36 -0400

    started Serializers

--- apps/user/models.py
+++ apps/user/models.py
@@ -10,8 +10,13 @@ class User(models.Model):
     class Meta(object):
         db_table = 'user'
 
-    name = models.CharField('Name', max_length = 50, blank=False, null=False) 
-    email = models.CharField('Email', max_length = 50, blank=False, null=False)
-    password = models.CharField('Password', max_length=50, blank=False, null=False)
-    updated_at = models.DateTimeField('Update_at',  blank=True, auto_now_add=True)
+    name = models.CharField('Name', max_length = 50, blank=False, null=False , db_index = True) 
+    email = models.EmailField('Email', max_length = 50, blank=False, null=False, db_index = True)
+    password = models.CharField('Password', max_length=50, blank=False, null=False, db_index = True)
+    token = models.CharField('Token', max_length=500, blank=True, db_index=True)
+    updated_at = models.DateTimeField('Update_at',  blank=True, auto_now=True)
     created_at = models.DateTimeField('Created_at', blank=True, auto_now_add=True)
+    token_expires = models.DateTimeField('Token_expires', blank=True, null=True)
+
+
+# JSON data -> javascript oject notation
---
+++ apps/user/serializers.py
@@ -0,0 +1,52 @@
+from .models import User
+from rest_framework import serializers 
+import datetime
+from secrets import token_hex
+from django.contrib.auth.hashers import make_password, check_password
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True) 
+
+    class Meta:
+        models=User
+        fields=('name','email','password','token','token_expires')
+
+class UserSignUpSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    token =  serializers.CharField(read_only=True)
+    token_expires = serializers.DateTimeField(read_only=True)
+
+    class Meta:
+        models=User
+        fields=('name','email','password','token','token_expires')
+    
+    def create(self, validated_data):
+        #encrypt the password
+        validated_data['password']= make_password(validated_data['password'])
+        #create a token
+        validated_data['token']= token_hex(30)
+        validated_data['token_expires']=datetime.datetime.now()+datetime.timedelta(days=7)
+        return super().create(validated_data)
+
+
+class UserSignInSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(read_only=True)
+    password = seriaizers.CharField(write_only=True)
+    token = serializers.CharField(read_only=True)
+    token_expires = serializers.DateTimeField(read_only=True)
+    email = serializers.EmailField(read_only=True)
+
+    class Meta:
+        models=User
+        fields=('name','email','password','token','token_expires')
+
+    def create (self, validated_data):
+        user=User.objects.filter(email=validated_data['email']) 
+        #check the password
+        if len(user)>0 and check_password(validated_data['password'], user[0].password):
+            user[0].token=token_hex(30)
+            user[0].token_expires=datetime.datetime.now()+datetime.timedelta(days=7) 
+            user[0].save()
+            return user[0] 
+        else:
+            raise seriaizers.ValidationError({'error':'The password or email is incorrect'})
